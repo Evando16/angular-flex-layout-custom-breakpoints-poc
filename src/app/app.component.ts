fbe05f47f6b604e4f9e05f0f6589eb3986648e07
E. Junior <evando@ciandt.com>
2021-01-13 13:09:51 -0300

    Custom breakpoints to WXGA+ and WSXGA+ resolutions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,8 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
   <div fxLayout="row" fxLayoutAlign="center center">
-    <span style="background-color: red" fxFlex.lg.custom="20" fxFlex="50">test 1</span>
-    <span style="background-color: orange"fxFlex.lg="50">test 2</span>
+    <span style="background-color: red" fxFlex.lg="50" fxFlex.lg-wxga-plus="80" fxFlex.lg-wsxga-plus="30">test 1</span>
+    <span style="background-color: orange"fxFlex>test 2</span>
   </div>
   `,
   styleUrls: ['./app.component.scss']
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, Directive, Optional, ElementRef } from '@angular/core';
-import { FlexLayoutModule, StyleUtils, MediaMarshaller, LayoutDirective, LayoutStyleBuilder, BREAKPOINTS, DEFAULT_BREAKPOINTS, BREAKPOINT } from '@angular/flex-layout';
+import { NgModule, Directive } from '@angular/core';
+import { FlexLayoutModule, FlexDirective } from '@angular/flex-layout';
 import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,39 +8,33 @@ import { AppComponent } from './app.component';
 
 const CUSTOM_BREAKPOINTS = [
   {
-    alias: 'lg.custom',
-    mediaQuery: 'screen and (min-width: 1440px) and (max-width: 1679.9px)',
-    suffix: 'custom',
+    alias: 'lg',
+    mediaQuery: 'screen and (min-width: 1280px) and (max-width: 1439.9px)',
+    suffix: 'Lg',
     overlapping: true
+  },
+  {
+    alias: 'lg-wxga-plus',
+    mediaQuery: 'screen and (min-width: 1440px) and (max-width: 1679.9px)',
+    suffix: 'LgWxgaPlus',
+    overlapping: false
+  },
+  {
+    alias: 'lg-wsxga-plus',
+    mediaQuery: 'screen and (min-width: 1680px) and (max-width: 1919.9px)',
+    suffix: 'LgWsxgaPlus',
+    overlapping: false
   }
 ];
 
-export const customBreakPointsProvider = {
-  provide: BREAKPOINT,
-  useValue: CUSTOM_BREAKPOINTS,
-  multi: true
-};
+const inputs = ['fxFlex.lg', 'fxFlex.lg-wxga-plus', 'fxFlex.lg-wsxga-plus'];
+const selector = `[fxFlex.lg], '[fxFlex.lg-wxga-plus]', '[fxFlex.lg-wsxga-plus]'`;
 
-@Directive({
-  selector: `[fxFlex.lg.custom]`
-})
-export class CustomShowHideDirective extends LayoutDirective {
-  constructor(protected elRef: ElementRef,
-    protected styleUtils: StyleUtils,
-    @Optional() protected styleBuilder: LayoutStyleBuilder,
-    protected marshal: MediaMarshaller) {
-    super(elRef, styleUtils, styleBuilder, marshal);
-  }
-
-  /**
-   * I could not reproduce the follow method as described in custom breakpoint documentation
-   * I imagine that we should have another method to be used with LayoutDirective, but could found any solution
-   */
-  // @Input('fxHide.xs.print')
-  // set hideXs(val) {
-  //   this._cacheInput("showXsPrint", negativeOf(val));
-  // }
+@Directive({ inputs, selector })
+export class CustomShowHideDirective extends FlexDirective {
+  protected inputs = inputs;
 }
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -50,10 +44,7 @@ export class CustomShowHideDirective extends LayoutDirective {
     CommonModule,
     BrowserModule,
     AppRoutingModule,
-    FlexLayoutModule,
-  ],
-  providers: [
-    customBreakPointsProvider
+    FlexLayoutModule.withConfig({}, CUSTOM_BREAKPOINTS),
   ],
   bootstrap: [AppComponent]
 })

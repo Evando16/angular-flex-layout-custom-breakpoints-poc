e0394bf65a6acf117c82f325b8faa50336896244
E. Junior <evando@ciandt.com>
2021-01-13 12:02:40 -0300

    Project start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <div fxLayout="row" fxLayoutAlign="center center">
+    <span style="background-color: red" fxFlex.lg.custom="20" fxFlex="50">test 1</span>
+    <span style="background-color: orange"fxFlex.lg="50">test 2</span>
+  </div>
+  `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,60 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Directive, Optional, ElementRef } from '@angular/core';
+import { FlexLayoutModule, StyleUtils, MediaMarshaller, LayoutDirective, LayoutStyleBuilder, BREAKPOINTS, DEFAULT_BREAKPOINTS, BREAKPOINT } from '@angular/flex-layout';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+const CUSTOM_BREAKPOINTS = [
+  {
+    alias: 'lg.custom',
+    mediaQuery: 'screen and (min-width: 1440px) and (max-width: 1679.9px)',
+    suffix: 'custom',
+    overlapping: true
+  }
+];
+
+export const customBreakPointsProvider = {
+  provide: BREAKPOINT,
+  useValue: CUSTOM_BREAKPOINTS,
+  multi: true
+};
+
+@Directive({
+  selector: `[fxFlex.lg.custom]`
+})
+export class CustomShowHideDirective extends LayoutDirective {
+  constructor(protected elRef: ElementRef,
+    protected styleUtils: StyleUtils,
+    @Optional() protected styleBuilder: LayoutStyleBuilder,
+    protected marshal: MediaMarshaller) {
+    super(elRef, styleUtils, styleBuilder, marshal);
+  }
+
+  /**
+   * I could not reproduce the follow method as described in custom breakpoint documentation
+   * I imagine that we should have another method to be used with LayoutDirective, but could found any solution
+   */
+  // @Input('fxHide.xs.print')
+  // set hideXs(val) {
+  //   this._cacheInput("showXsPrint", negativeOf(val));
+  // }
+}
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomShowHideDirective
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule,
   ],
-  providers: [],
+  providers: [
+    customBreakPointsProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
